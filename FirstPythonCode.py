0f1750a1a24e2a14aee3677836294d9346ef07bb
keshavkummari <keshav.kummari@gmail.com>
2018-12-06 08:07:37 +0530

    Add files via upload

---
+++ FirstPythonCode.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+
+# Comments :
+
+	# 1. Single Line : 1. '', 2. " ", 3. ''' ''', 4. """ """ & 5. # 
+	# 2. Multi Line  : 1. ''' ''' & 2. """ """
+
+"Notes" 
+
+print("Hi, Welcome to Python World")
+
+print("Today is our Day-1 Demo")
+
+
+
+
