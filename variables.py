9137cf572da4e657aef0a7d16f54468961f604d7
keshavkummari <keshav.kummari@gmail.com>
2018-12-07 07:50:50 +0530

    Variables

---
+++ variables.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+'''
+Comments :
+
+1. Single Comments    : '', "", ''' ''', """ """ & #
+2. Multiline Comments : ''' ''', & """ """
+'''
+
+# Creating Variables in Python
+
+'Rule of Creating Variables in python'
+
+"""
+1. A-Z
+2. a-z
+3. A-Za-z
+4. _ 
+5. 0-9
+6. Note : We can not create a variable name with numeric Value as a prefix
+"""
+
+'''left operand = right operand
+left operand : Name of the Variable 
+right operand : Value of the Variable''' 
+
+"Creating variables in Python"
+
+FIRSTNAME = 'Guido'
+middlename = "Van"
+LASTname = '''Rossum'''
+_python_lang = """Python Programming Language"""
+
+"Accessing Variables in Python"
+
+print(FIRSTNAME)
+print("")
+
+print(middlename)
+print("")
+
+print(LASTname)
+print("")
+
+print(_python_lang)
+
+
+
