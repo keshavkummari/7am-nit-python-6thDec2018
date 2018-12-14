a26e956e2c565b0937ccafab57c1fcb93645aca9
keshavkummari <keshav.kummari@gmail.com>
2018-12-14 08:10:57 +0530

    basics

---
+++ datatypes/basics/comments.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+#Single Line : # , '' , "" , ''' ''', and """ """
+
+#Multi Line Comments : ''' ''', and """ """
+
+# Creating Variables in Python
+
+firstname = 'Guido'
+
+middlename = 'Van'
+
+lastname = "Rossum"
+
+print(firstname)
+
+print(middlename)
+
+print(lastname)
