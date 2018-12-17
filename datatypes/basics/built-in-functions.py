a0ca61f16dbcc76d1e78e289cfcc51d0ce53ec29
y2it299 <ktkmsrinivas@gmail.com>
2018-12-17 23:02:37 +0530

    Built in functions

--- datatypes/basics/built-in-functions.py
+++ datatypes/basics/built-in-functions.py
@@ -1,91 +1,18 @@
-# Creating Variables in Python :
+FIRSTNAME = 'Guido'
+middlename = "Van"
+LastName = '''Rosum'''
 
-#firstname = 'Guido'
-middlename = 'Van'
-lastname = "Rossum"
+# Accessing variables in python
+print("Calling a variable firstname:", FIRSTNAME)
+print(middlename, "We have called a variable middle")
+print("Calling variable", LastName, "LastName")
 
-# Accesing Variables in Python :
-
-#print(firstname)
-#print("Calling a Variable i.e. FirstName : ", firstname)
-#print(firstname,"We have called a Variable call Firstname")
-#print("Calling Variable",firstname,"Using Print Function")
-
-# String Special Operators :
-
-'''
-1. + : Concatenation
-
-2. * : Repetition
-
-3. []: Slice
-
-4. [:]: Range Slice
-
-5. [::] : Zero Based Indexing
-
-6. % : Format
-
-7. .format()
-'''
-
-#names = firstname + lastname
-
-#print(names)
-'''
-Indexing 
-
-Left to Right
-0 1 2 3 4 5 nth 
-
-Right to Left
--nth -5 -4 -3 -2 -1
-'''
-           #  0   1   2  3   4 Left to Right Indexing
-           # -5  -4  -3 -2  -1 Right to Left Indexing
-#firstname=' G   u   i  d   o'
-
-firstname = 'Guido Van Rossum'
-
-print('Left to Right Indexing : ',firstname[0])
-print('Right to Left Indexing : ',firstname[-5])
-
-# print('Range Slice[:]',firstname[Start index: end index]) end index -1 :
-
-print('Range Slice[:]',firstname[2:5])
-
-print('Range Slice[:]',firstname[1:]) # endindex-1 = 3 (0123)
-
-           # 012121212121212121
-numValues = '102030405060708090'
-
-print("Zero Based Indexing",numValues[2::4])
-
-"""
-Below are the list of complete set of symbols which can be used along with % :
-
-Format Symbol	Conversion
-%c 				character
-%s 				string conversion via str()
-                                prior to formatting
-%i 				signed decimal integer
-%d 				signed decimal integer
-%u 				unsigned decimal integer
-%o 				octal integer
-%x 				hexadecimal integer (lowercase letters)
-%X 				hexadecimal integer (UPPERcase letters)
-%e 				exponential notation (with lowercase 'e')
-%E 				exponential notation (with UPPERcase 'E')
-%f 				floating point real number
-%g 				the shorter of %f and %e
-%G 				the shorter of %f and %E
-"""
-
-print("FirstName : %s  MiddleName : %s  LastName: %s " %(firstname,middlename,lastname))
-print("FirstName :",firstname,"MiddleName : ",middlename,"LastName:",lastname)
-
-print("FirstName : {}  MiddleName : {}  LastName: {} ".format(firstname,middlename,lastname))
-
-# raw sring : r/R r'expression' or R'expression'
-print(r'c:\\users\\keshavkummari\\')
-print(R'c:\\users\\keshavkummari\\')
+names = FIRSTNAME + LastName
+print(names)
+middles = middlename * 5
+print(middles)
+print(FIRSTNAME[1])
+print(FIRSTNAME[-1])
+print(FIRSTNAME[1:5])
+print(FIRSTNAME[2:4])
+print(FIRSTNAME[1::2])
