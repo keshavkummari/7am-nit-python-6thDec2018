a1ba7075e584700a3ed5423947b10d09fd12d60b
y2it299 <ktkmsrinivas@gmail.com>
2018-12-17 21:50:45 +0530

    HelloWorld

---
+++ HelloWorld.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+# Comments
+'''
+1. Single Line : 1. '', 2. "", 3.''' '''
+2. Multi Line : 1.''' ''' and 2.""" '""
+'''
+
+print("Hi, Welcome to Phyton World")
+
+print("Today is our Day-1 Demo")
---
+++ Variables.py
@@ -0,0 +1,22 @@
+# Variables
+
+FIRSTNAME = 'Guido'
+middlename = "Van"
+LastName = '''Rosum'''
+_python_lang = """Python Programming Language"""
+
+print(FIRSTNAME)
+#line break
+print("")
+
+print(middlename)
+#line break
+print("")
+
+print(LastName)
+#line break
+print("")
+
+print(_python_lang)
+#line break
+print("")
