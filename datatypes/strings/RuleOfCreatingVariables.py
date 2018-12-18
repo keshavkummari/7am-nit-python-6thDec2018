a7abcd5836e36197c6edc5b82b2c872285929b13
keshavkummari <keshav.kummari@gmail.com>
2018-12-18 08:01:27 +0530

    methods

---
+++ datatypes/strings/RuleOfCreatingVariables.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+# Rule of Creating Variables in Python
+
+'''
+1. A-Z
+2. a-z
+3. 0-9
+4. _
+5. A-Za-z
+6. A-Za-z0-9
+7. A-Za-z0-9_
+'''
+
+# Creating Variables in Python
+FIRSTNAME = "Guido"
+middlename = "Van"
+LastName = "Rossum"
+_language = "Python"
+Varsion_number = 3.7
+_007 = "James Bond"
+
+print(FIRSTNAME,middlename,LastName,_language,Varsion_number,_007)
+
+
+
+
+
