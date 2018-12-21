ea2637c4bc46e297a44dffa0e8a1138a1383d4c0
keshavkummari <keshav.kummari@gmail.com>
2018-12-21 08:01:16 +0530

    Tuple-List

---
+++ datatypes/list/List_Examples.py
@@ -0,0 +1,12 @@
+
+
+
+list1 = ['python', 'linux', 1997, 20.00]
+list2 = [1, 2, 3, 4, 5 ]
+list3 = ["a", "b", "c", "d"]
+
+print(list1,type(list1),id(list1),len(list1),list(enumerate(list1)))
+
+print(list2,type(list2),id(list2),len(list2),list(enumerate(list2)))
+
+print(list3,type(list3),id(list3),len(list3),list(enumerate(list3)))
---
+++ datatypes/tuple/Tuple_Methods.py
@@ -0,0 +1,71 @@
+tup1 = ("aws",'azure',1997,2050,50.75)
+
+tup2 = (1,2,3,4,5,6,7)
+
+print(tuple(enumerate(tup1)),type(tup1),id(tup1),len(tup1))
+
+print(tuple(enumerate(tup2)),type(tup2),id(tup2),len(tup2))
+
+print(tup1[:5])  # startindex:endindex (endindex-1; 5-1=4)
+
+del tup1
+
+# del tup1[0] # TypeError: 'tuple' object doesn't support item deletion
+
+print(tup1)#!/usr/bin/python
+
+# tuple1, tuple2 = (123, 'xyz', 'minnu'), (456, 'abc')
+
+tuple1=(123, 'xyz', 'minnu')
+tuple2=(456, 'abc')
+
+"""
+print ("First tuple length : ", len(tuple1),type(tuple1))
+print ("Second tuple length : ", len(tuple2))
+"""
+tuple1, tuple2 = ('123', 'xyz', 'minnu', 'abc'), (456, 700, 200)
+
+print ("Min value element : ", min(tuple1))
+print ("Max value element : ", max(tuple1))
+
+print ("Min value element : ", min(tuple2))
+print ("Max value element : ", max(tuple2))
+
+
+aList = [123, 'xyz', 'minnu', 'abc']
+aTuple = tuple(aList)
+
+print ("Tuple elements : ",type(aTuple),aTuple)
+
+print("Convert tuple into list",list(aTuple))
+#!/usr/bin/python
+
+# tuple1, tuple2 = (123, 'xyz', 'minnu'), (456, 'abc')
+
+tuple1=(123, 'xyz', 'minnu')
+tuple2=(456, 'abc')
+
+"""
+print ("First tuple length : ", len(tuple1),type(tuple1))
+print ("Second tuple length : ", len(tuple2))
+"""
+tuple1, tuple2 = ('123', 'xyz', 'minnu', 'abc'), (456, 700, 200)
+
+print ("Min value element : ", min(tuple1))
+print ("Max value element : ", max(tuple1))
+
+print ("Min value element : ", min(tuple2))
+print ("Max value element : ", max(tuple2))
+
+
+aList = [123, 'xyz', 'minnu', 'abc']
+aTuple = tuple(aList)
+
+print ("Tuple elements : ",type(aTuple),aTuple)
+
+print("Convert tuple into list",list(aTuple))
+
+
+tup = (1,2, [1,3])
+
+print(tup,type(tup),tuple(enumerate(tup)))
