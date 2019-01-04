4d3ef909e3e3aa78205e8b80b64970e71de70144
keshavkummari <keshav.kummari@gmail.com>
2019-01-04 09:43:38 +0530

    assignments

---
+++ examples/assignments_1/strings.py
@@ -0,0 +1,11 @@
+
+1. Write a Python program to calculate the length of a string.
+
+2. Write a Python program to count the number of characters (character frequency) in a string.
+
+3. Write a Python program to get the last part of a string before a specified character.
+
+4. Write a Python program to check whether a string starts with specified characters.
+
+5. Write a Python program to print the index of the character in a string.
+
