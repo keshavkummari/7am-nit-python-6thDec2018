46d81ea3613a597592c3b5350f659d5b90dcc469
keshavkummari <keshav.kummari@gmail.com>
2019-01-05 10:02:06 +0530

    functions-assignments

---
+++ examples/assignments_1/functions_combinationsAssignments.py
@@ -0,0 +1,9 @@
+1. Function to find the Max of three numbers.
+
+2. Function to sum all the numbers in a list.
+
+3. Write a Python function to multiply all the numbers in a list.
+
+4. Program to access a function inside a function.
+
+5. Function that accepts a string and calculate the number of upper case letters and lower case letters.
