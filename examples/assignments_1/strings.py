6bbd68573c4030c18aa8c4897444b37d275dfe25
AgniChegondi <agni345@gmail.com>
2019-01-05 17:14:38 +0530

    assignments-agni

--- examples/assignments_1/strings.py
+++ examples/assignments_1/strings.py
@@ -1,11 +1,24 @@
 
-1. Write a Python program to calculate the length of a string.
+#1. Write a Python program to calculate the length of a string.
+str2 = 'Agni Parameswara Rao Chegondi'
+print(len(str2))
 
-2. Write a Python program to count the number of characters (character frequency) in a string.
+#2. Write a Python program to count the number of characters (character frequency) in a string.
+print(str2.count('a'))
+print(str2.count('e'))
 
-3. Write a Python program to get the last part of a string before a specified character.
+#3. Write a Python program to get the last part of a string before a specified character.
+special_char = 'R'
+print(str2[str2.index(special_char):])
 
-4. Write a Python program to check whether a string starts with specified characters.
+#4. Write a Python program to check whether a string starts with specified characters.
+print(str2.startswith('Agni'))
+print(str2.endswith('Chegondi'))
 
-5. Write a Python program to print the index of the character in a string.
+
+#5. Write a Python program to print the index of the character in a string.
+special_char2 = 'i'
+print(str2.index(special_char2))
+
+
 
