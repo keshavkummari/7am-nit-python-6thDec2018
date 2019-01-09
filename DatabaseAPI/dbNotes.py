9833766da9b42999f65bb037d938b8252c06e013
keshavkummari <keshav.kummari@gmail.com>
2019-01-09 09:40:32 +0530

    db

--- DatabaseAPI/dbNotes.py
+++ DatabaseAPI/dbNotes.py
@@ -7,3 +7,14 @@ for row in csv_data:
 	c.execute("INSERT INTO a (first, last) VALUES (%s, %s), row")
 c.commit()
 c.close()
+
+
+import MySQLdb, csv, sys
+conn = MySQLdb.connect (host = "localhost",user = "usr", passwd = "pass",db = "db")
+c = conn.cursor()
+csv_data=csv.reader(file("b.txt"))
+for row in csv_data:
+    print row
+    c.execute("UPDATE a SET last = %s", row)
+#c.commit()
+c.close()
